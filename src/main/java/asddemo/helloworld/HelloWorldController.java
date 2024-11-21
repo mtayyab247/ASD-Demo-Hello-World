712ab8cb9384b3d64f3e07eb5c5cb7c4b72a5238
mtayyab247 <mtayyab@miu.edu>
2024-11-20 20:41:29 -0600

    first commit

---
+++ src/main/java/asddemo/helloworld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package asddemo.helloworld;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("")
+    public ResponseEntity<String> home() {
+        return new ResponseEntity<>("Hello world!", HttpStatus.OK);
+    }
+}
